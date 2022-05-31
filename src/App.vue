d5a3d27ac1e242af14c133b0438a0c1bee6f5dcc
AprilEcho <18813314680@163.com>
2022-05-31 19:34:33 +0800

    feat(test): 项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <h2>{{ (this as any).$store.state.name }}</h2>
+    <h2>{{ $store.state.name }}</h2>
     <router-link to="/login">登录</router-link>
     <router-link to="/main">首页</router-link>
     <router-view />
@@ -9,9 +9,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useStore } from 'vuex'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    const $store = useStore()
+    return {
+      $store
+    }
+  }
 })
 </script>
 
