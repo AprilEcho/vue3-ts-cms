d8e2ccf193dca1a221ad2737febe8b9f6af8e4f4
AprilEcho <18813314680@163.com>
2022-05-31 19:11:36 +0800

    feat(test): 项目初始化

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
-
-console.log('1111')
+import router from './router'
+import store from './store'
+createApp(App).use(router).use(store).mount('#app')
---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'llz'
+    }
+  }
+})
+export default store
