89662615e9ec80d3ba696c2f52a62ade977afee5
AprilEcho <18813314680@163.com>
2022-06-09 15:17:29 +0800

    feat: 路由映射

--- src/components/nav-menu/src/nav-menu.vue
+++ src/components/nav-menu/src/nav-menu.vue
@@ -2,28 +2,35 @@
   <div class="nav-menu">
     <div class="logo">
       <img class="img" src="~@/assets/img/logo.svg" alt="logo" />
-      <span class="title">Vue3+TS</span>
+      <span v-if="!collapse" class="title">Vue3+TS</span>
     </div>
     <!-- 二级菜单 -->
-    <el-menu default-active="2" class="el-menu-vertical">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical"
+      background-color="#0c2135"
+      text-color="#b7bdc3"
+      active-text-color="#0a60bd"
+      :collapse="collapse"
+    >
       <template v-for="item in userMenus" :key="item.id">
         <template v-if="item.type === 1">
-          <el-submenu>
+          <el-submenu :index="item.id + ''">
             <template #title>
               <i v-if="item.icon" :class="item.icon"></i>
               <span>{{ item.name }}</span>
             </template>
+            <template v-for="subitem in item.children" :key="subitem.id">
+              <el-menu-item :index="subitem.id + ''">
+                <i v-if="subitem.icon" :class="subitem.icon"></i>
+                <span>{{ subitem.name }}</span>
+              </el-menu-item>
+            </template>
           </el-submenu>
-          <template v-for="subitem in item.children" :key="subitem.id">
-            <el-menu-item>
-              <i v-if="subitem.icon" :class="subitem.icon"></i>
-              <span>{{ subitem.name }}</span>
-            </el-menu-item>
-          </template>
         </template>
         <!-- 一级菜单 -->
         <template v-else-if="item.type === 2">
-          <el-menu-item>
+          <el-menu-item :index="item.id + ''">
             <i v-if="item.icon" :class="item.icon"></i>
             <span>{{ item.name }}</span>
           </el-menu-item>
@@ -37,10 +44,16 @@
 import { defineComponent } from 'vue'
 import { useStore } from '../../../store'
 export default defineComponent({
+  props: {
+    collapse: {
+      type: Boolean,
+      default: false
+    }
+  },
   setup() {
     const store = useStore()
     const userMenus = store.state.LoginModule.userMenus
-    console.log(userMenus)
+    // console.log(userMenus)
 
     return { userMenus }
   }
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="main">
     <el-container class="main-content">
-      <el-aside width="210px"> <NavMenu /> </el-aside>
+      <el-aside :width="isCollapse ? '60px' : '210px'">
+        <NavMenu :collapse="isCollapse" />
+      </el-aside>
       <el-container class="page">
-        <el-header class="page-header"> headert </el-header>
+        <el-header class="page-header">
+          <nav-header @foldChange="handleFoldChange"></nav-header>
+        </el-header>
         <el-main class="page-content"> main </el-main>
       </el-container>
     </el-container>
@@ -11,14 +15,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import NavMenu from '../../components/nav-menu'
+import { defineComponent, ref } from 'vue'
+import NavMenu from '@/components/nav-menu'
+import NavHeader from '@/components/nav-header'
 export default defineComponent({
   components: {
-    NavMenu
+    NavMenu,
+    NavHeader
   },
   setup() {
-    return {}
+    const isCollapse = ref(false)
+    const handleFoldChange = (isFold: boolean) => {
+      isCollapse.value = isFold
+    }
+    return {
+      isCollapse,
+      handleFoldChange
+    }
   }
 })
 </script>
